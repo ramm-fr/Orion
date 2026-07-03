1c64b94674393b8b0e2824690f3cb5ab69fcb896
Ram <ram@orion-player.dev>
2026-07-03 18:09:42 +0530

    Update app ID to io.github.ramm_fr.Orion for Flathub

--- orion/__init__.py
+++ orion/__init__.py
@@ -1,5 +1,5 @@
 """Orion Video Player - A GNOME-style media player for Linux."""
 
 __version__ = "2.0.0"
-__app_id__ = "com.orion.player"
+__app_id__ = "io.github.ramm_fr.Orion"
 __app_name__ = "Orion"
